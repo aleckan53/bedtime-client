beb089646a2e729a4767c87e808b6caf835c3664
admin <admin@admins-MacBook-Pro-3.local>
2019-05-07 19:36:02 -0700

    fix Quill text typing issue on mobiles

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import ReactQuill from 'react-quill'
-import 'react-quill/dist/quill.bubble.css'
+import config from 'config'
+import Api from './api'
+import 'react-quill/dist/quill.snow.css'
+import './styles.css'
 
 const Editor = props => {
 
@@ -8,33 +11,66 @@ const Editor = props => {
     html: '',
     error: null,
   })
+  const [loading, setLoading] = useState(false)
+  const [message, setMessage] = useState('')
 
-  const handleChange = html => {
-    setState({ ...state, html })
+  const handleChange = (html) => {
+    setState({
+      ...state,
+      html,
+    })
+  } 
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setLoading(true)
+    Api.uploadStory(state.story)
+      .then(res => {
+        setMessage(res.message)
+        setState({
+          html: '',
+          error: null,
+        })
+        setLoading(false)
+        setTimeout(() => {
+          setMessage('')
+        }, 4e3)
+      })
+      .catch(error => {
+        setState({
+          ...state,
+          error
+        })
+      })
   }
+  const userAuthorized = !!window.sessionStorage.getItem(config.TOKEN_NAME)
+
 
   return (
-    <section className='Editor'>
+    <section className='editor'>
+      {!loading ? '' : <p className='msg'>Processing...</p>}
+      <div className='msg'>{message}</div>
       <ReactQuill
-        theme='bubble'
+        theme='snow'
         onChange={handleChange}
         value={state.html}
         bounds='.editor'
-        style={{
-          border: '1px solid white',
-          color: 'white',
-          cursor: 'text'
-        }}
-        // modules={modules}
-        // formats={formats}
-        // placeholder='Hello'
-        />
+        modules={modules}
+        formats={formats}
+        placeholder={placeholder}
+        > 
+      </ReactQuill>
+      <button className='submit' onClick={handleSubmit} disabled={!userAuthorized}>{userAuthorized ? 'Submit' : 'Please login'}</button>
     </section>
   )
 }
 
 export default Editor
 
+const placeholder = `Please use H1 for story's title\n
+Supported images types: *.png, *.jpeg. Others will be ignored\n
+Story has to be at least of 800 characters long ~ 1 min read
+`
 
 const modules = {
   toolbar: [
