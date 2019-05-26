f73021dd964a418f53300577ef9301bfc32971cd
aleckan53 <admin@admins-MacBook-Pro-3.local>
2019-05-25 19:38:11 -0700

    add mobile swipes to Stack component

--- src/components/Home/components/Stack/index.js
+++ src/components/Home/components/Stack/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Card from './components/Card'
 import Service from './service'
+import Swipe from 'react-easy-swipe'
 import './styles.css'
 import { 
   FaArrowLeft as Left,
@@ -11,25 +12,30 @@ import {
 const Stack = props => {
 
   return (
-    <div className='stack'>
-    <div className='top'>
-      <h1>{props.title}</h1>
-      <div className='controls'>
-        <button onClick={() => Service.handlePrev(props.title)}>
-          <Left/>
-        </button>
-        <div className='dots' id={props.title + '-dots'}>
-          {Service.mapIndicators(props.data.length, props.title)}
-        </div>
-        <button onClick={() => Service.handleNext(props.title)}>
-          <Right/>
-        </button>
-    </div>
+    <Swipe
+      onSwipeRight={() => Service.handlePrev(props.title)}
+      onSwipeLeft={() => Service.handleNext(props.title)}
+    >
+      <div className='stack'>
+      <div className='top'>
+        <h1>{props.title}</h1>
+        <div className='controls'>
+          <button onClick={() => Service.handlePrev(props.title)}>
+            <Left/>
+          </button>
+          <div className='dots' id={props.title + '-dots'}>
+            {Service.mapIndicators(props.data.length, props.title)}
+          </div>
+          <button onClick={() => Service.handleNext(props.title)}>
+            <Right/>
+          </button>
       </div>
-      <ul className='deck' id={props.title}>
-        {Service.mapStories(props.data, Card)}
-      </ul>
-    </div>
+        </div>
+        <ul className='deck' id={props.title}>
+          {Service.mapStories(props.data, Card)}
+        </ul>
+      </div>
+    </Swipe>
   )
 }
 
